e86fe024ccd7ef4a02b024cb45c1d1f410ee6a69
saheedibrahim <saolabram@gmail.com>
2022-04-14 04:34:57 +0100

    ubuntu project first pus

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        bgsideColor: '#FF3D51',
+        sideColor: '#FFD8DC',
+        bodycolor: '#FFD8DC',
+      },
+      borderRadius: {
+        sideRad: '45px ',
+      }
+    },
+  },
+  plugins: [],
+}
