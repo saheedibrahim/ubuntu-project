205678b51a6bae1c9d47646584b4ce27b114af29
saheedibrahim <saolabram@gmail.com>
2022-04-19 10:27:32 +0100

    dashboard edited

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
+      fontSize: {
+        'xxl': '9px',
+      },
       colors: {
         bgsideColor: '#FF3D51',
         sideColor: '#FFD8DC',
         bodycolor: '#FFD8DC',
+        homeColor: '#524CBF',
       },
       borderRadius: {
         sideRad: '45px ',
