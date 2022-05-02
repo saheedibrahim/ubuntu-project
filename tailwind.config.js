31b977d96ecabb378343f381aeb88837bc4a620c
saheedibrahim <saolabram@gmail.com>
2022-05-02 18:50:33 +0100

    started page 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,18 @@ module.exports = {
         sideM : '10px'
       },
       height: {
-        500: '500px'
+        500: '500px',
+        340: '340px',
       },
       width: {
         '691': '691px',
         '368': '368px',
-        24: '24%',
-        1: '1px'
+        '24p': '24%',
+        1: '1px',
+        596: '596px',
+        431: '431px',
+        110: '110px',
+        220: '220px'
       },
       fontSize: {
         'xxl': '9px',
@@ -28,7 +33,8 @@ module.exports = {
         homeColor: '#524CBF',
         voluntColor: '#3EC9ED',
         tarColor: '#524CBF',
-        vaultColor: '#4EBF31'
+        vaultColor: '#4EBF31',
+        calRep: '#FF3D5150',
       },
       borderRadius: {
         sideRad: '45px ',
